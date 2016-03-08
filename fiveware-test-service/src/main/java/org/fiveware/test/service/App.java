0af1d34bc7515d1916a48063791e061ac9fe410b
Italo Santana <italo.santss@gmail.com>
2016-03-08 01:18:56 -0300

    Estrutura basica do projeto - Spring sem XML + Maven

---
+++ fiveware-test-service/src/main/java/org/fiveware/test/service/App.java
@@ -0,0 +1,13 @@
+package org.fiveware.test.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldss class!" );
+    }
+}
---
+++ fiveware-test-web/src/main/java/org/fiveware/test/web/AppConfig/AppConfig.java
@@ -0,0 +1,30 @@
+package org.fiveware.test.web.AppConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages={"org.fiveware.test.web"})
+public class AppConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+	  InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	  resolver.setPrefix("/WEB-INF/");
+	  resolver.setSuffix(".jsp");
+	 
+	  return resolver;
+	}
+	
+}
